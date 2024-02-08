3b3f00dda00c5b92c076a50e02a5654c45ff3584
WayneRoseberryGit <wayne_roseberry@hotmail.com>
2024-02-07 19:20:25 -0800

    changing pages router to read file on its own and send contents

--- routes/pages.js
+++ routes/pages.js
@@ -1,13 +1,26 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 var path = require('path');
 
 /* GET home page. */
 router.get('/*', function (req, res) {
-    var fileName = path.join(__dirname, req.path);
-    console.log(`pages routing ${req.path}, opening ${fileName}`);
-    res.sendFile(fileName);
+    var pagesdir = path.join(__dirname, '..\\pages');
+    var fileName = path.join(pagesdir, req.path);
+    console.log(` GET pages routing ${req.path}, opening ${fileName}, originalurl:${req.originalUrl}`);
+
+    fs.readFile(fileName, { encoding: 'utf-8' }, function (err, data) {
+        if (!err) {
+            console.log(`received data: ${data}`);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        }
+        else {
+            console.log(err);
+        }
+    });
 
 });
 
