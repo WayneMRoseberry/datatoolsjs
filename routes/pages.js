36f81c25b993b573872514e0f1becb2fa59c8ad9
WayneRoseberryGit <wayne_roseberry@hotmail.com>
2024-02-07 19:31:33 -0800

    changing logging to be less verbose

--- routes/pages.js
+++ routes/pages.js
@@ -12,7 +12,6 @@ router.get('/*', function (req, res) {
 
     fs.readFile(fileName, { encoding: 'utf-8' }, function (err, data) {
         if (!err) {
-            console.log(`received data: ${data}`);
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(data);
             res.end();
